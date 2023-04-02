1c5ee3a305b7c3a5efed7724282ec954f7619439
Malacar-Miles <105152910+Malacar-Miles@users.noreply.github.com>
2023-04-02 23:58:49 +0200

    Moved some helper function from calculatorSlice into a separate file as per review feedback.

--- src/utils/redux/calculator-slice.ts
+++ src/utils/redux/calculator-slice.ts
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "./store";
+import { toDisplayValue, toNumericValue, calculate } from "./helper-functions";
 import {
   KeypadNumericInput,
   KeypadOperatorInput,
@@ -68,57 +69,11 @@ const calculatorSlice = createSlice({
       const currentOperatorInput = action.payload;
       const currentOperatorIsEquals = currentOperatorInput === "=";
 
-      const calculate = (a: number, operator: MathOperator, b: number) => {
-        switch (operator) {
-          case "x":
-            return a * b;
-          case "+":
-            return a + b;
-          case "-":
-            return a - b;
-          case "/":
-            if (b !== 0) return a / b;
-            else return null;
-        }
-      };
-
       // This helper function will be used whenever we need to display
       // calculation results.
       const updateDisplay = () => {
         if (state.storedValue === null) state.displayValue = "Не определено";
         else state.displayValue = toDisplayValue(state.storedValue);
-      };
-
-      // This helper function converts a numeric value to a string
-      // and formats this string so it fits the display.
-      const toDisplayValue = (numericValue: number): string => {
-        const maxDisplayLength = 17;
-        const stringValue = numericValue.toString();
-        const numDigits = stringValue.length;
-
-        const truncateNumericValue = (longNumber: number) => {
-          // We truncate differently depending on whether the
-          // number is very big or just has a long decimal part
-          if (longNumber > -1 && longNumber < 1)
-            return longNumber.toFixed(maxDisplayLength - 1);
-          else {
-            const truncatedString = longNumber.toPrecision(maxDisplayLength - 1);
-            const isExponential = truncatedString.includes("e");
-            if (!isExponential) return truncatedString;
-              else return longNumber.toPrecision(3);
-          }
-        };
-
-        const result =
-          numDigits <= maxDisplayLength
-            ? stringValue
-            : truncateNumericValue(numericValue);
-        return result.replace(".", ",");
-      };
-
-      const toNumericValue = (stringValue: string): number => {
-        const localizedString = stringValue.replace(",", ".");
-        return Number(localizedString);
       };
 
       // If state.currentNumericInput isn't null, convert it to a number
---
+++ src/utils/redux/helper-functions.ts
@@ -0,0 +1,47 @@
+import { MathOperator } from "../types/types-and-constants";
+
+export const calculate = (a: number, operator: MathOperator, b: number) => {
+  switch (operator) {
+    case "x":
+      return a * b;
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "/":
+      if (b !== 0) return a / b;
+      else return null;
+  }
+};
+
+// This helper function converts a numeric value to a string
+// and formats this string so it fits the display.
+export const toDisplayValue = (numericValue: number): string => {
+  const maxDisplayLength = 17;
+  const stringValue = numericValue.toString();
+  const numDigits = stringValue.length;
+
+  const truncateNumericValue = (longNumber: number) => {
+    // We truncate differently depending on whether the
+    // number is very big or just has a long decimal part
+    if (longNumber > -1 && longNumber < 1)
+      return longNumber.toFixed(maxDisplayLength - 1);
+    else {
+      const truncatedString = longNumber.toPrecision(maxDisplayLength - 1);
+      const isExponential = truncatedString.includes("e");
+      if (!isExponential) return truncatedString;
+        else return longNumber.toPrecision(3);
+    }
+  };
+
+  const result =
+    numDigits <= maxDisplayLength
+      ? stringValue
+      : truncateNumericValue(numericValue);
+  return result.replace(".", ",");
+};
+
+export const toNumericValue = (stringValue: string): number => {
+  const localizedString = stringValue.replace(",", ".");
+  return Number(localizedString);
+};
